2e030971e5810e904bf541f4c8fa9d42073c12e6
Nicholas Brink <nicbrink@gmail.com>
2023-02-01 20:13:29 +0000

    changes API code

--- script.js
+++ script.js
@@ -1,8 +1,73 @@
+// Leaflet api for map
+var OpenStreetMap = L.tileLayer(
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }
+);
+
+window.onload = function () {
+  var map = L.map("map", {
+    center: [-27.743040084838867, 23.01875114440918],
+    zoom: 13,
+    // Values are x and y here instead of lat and long elsewhere.
+    maxBounds: [
+      [-120, -220],
+      [120, 220],
+    ],
+    layers: [OpenStreetMap],
+  });
+
+  var baseMaps = {
+    "OSM Mapnick": OpenStreetMap,
+  };
+
+  L.control.layers(baseMaps).addTo(map);
+
+  map.addLayer(OpenStreetMap);
+};
+
+// currency exchange api - list quotes
+// const options = {
+//     method: 'GET',
+//     url: 'https://currency-exchange.p.rapidapi.com/listquotes',
+//     headers: {
+//         'X-RapidAPI-Key': '12226b39a9mshfa23fe1cdb616ccp10fb0bjsnd8f20f98485c',
+//         'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
+//     }
+//     };
+
+//     axios.request(options).then(function (response) {
+//         console.log(response.data);
+//     }).catch(function (error) {
+//         console.error(error);
+//     });
+
+// currency exchange api - exchange
+const options2 = {
+  method: "GET",
+  url: "https://currency-exchange.p.rapidapi.com/exchange",
+  params: { from: "SGD", to: "MYR", q: "1.0" },
+  headers: {
+    "X-RapidAPI-Key": "12226b39a9mshfa23fe1cdb616ccp10fb0bjsnd8f20f98485c",
+    "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
+  },
+};
+
+axios
+  .request(options2)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+
 // Travel Places API - JS Axois
 
-import axios from "axios";
-
-const options = {
+const options3 = {
   method: "POST",
   url: "https://travel-places.p.rapidapi.com/",
   headers: {
@@ -12,7 +77,7 @@ const options = {
 };
 
 axios
-  .request(options)
+  .request(options3)
   .then(function (response) {
     console.log(response.data);
   })
@@ -29,7 +94,7 @@ axios
 //Your API key: 33298605-dbd0a27598c1d9d88adc9dbe1
 // PIXELBAY API-Host: https://pixabay.com/api/
 
-const options = {
+const options4 = {
   method: "POST",
   url: "https://pixabay.com/api/",
   headers: {
@@ -54,66 +119,3 @@ $.getJSON(URL, function (data) {
     });
   else console.log("No hits");
 });
-
-// Leaflet api for map
-var OpenStreetMap = L.tileLayer(
-    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    {
-      maxZoom: 19,
-      attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }
-  );
-  
-  window.onload = function() {
-    var map = L.map("map", {
-      center: [-27.743040084838867, 23.01875114440918],
-      zoom: 13,
-      // Values are x and y here instead of lat and long elsewhere.
-      maxBounds: [[-120, -220], [120, 220]],
-      layers: [OpenStreetMap
-              ]
-    })
-
-    var baseMaps = {
-        "OSM Mapnick": OpenStreetMap
-      };
-    
-      L.control.layers(baseMaps).addTo(map);
-
-      map.addLayer(OpenStreetMap);
-    };
-
-// currency exchange api - list quotes
-// const options = {
-//     method: 'GET',
-//     url: 'https://currency-exchange.p.rapidapi.com/listquotes',
-//     headers: {
-//         'X-RapidAPI-Key': '12226b39a9mshfa23fe1cdb616ccp10fb0bjsnd8f20f98485c',
-//         'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
-//     }
-//     };
-    
-//     axios.request(options).then(function (response) {
-//         console.log(response.data);
-//     }).catch(function (error) {
-//         console.error(error);
-//     });
-
-// currency exchange api - exchange
-const options2 = {
-    method: 'GET',
-    url: 'https://currency-exchange.p.rapidapi.com/exchange',
-    params: {from: 'SGD', to: 'MYR', q: '1.0'},
-    headers: {
-      'X-RapidAPI-Key': '12226b39a9mshfa23fe1cdb616ccp10fb0bjsnd8f20f98485c',
-      'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
-    }
-  };
-  
-  axios.request(options2).then(function (response) {
-      console.log(response.data);
-  }).catch(function (error) {
-      console.error(error);
-  });
-

f098d764261a519ed3e3518e6eda4abc96c3f230
Ope <o_orekoya@yahoo.com>
2023-02-02 18:51:25 +0000

    add weather to html and connect to javascript

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
-// Leaflet api for map
-
-var lat = -27.743040084838867
-var long = 23.01875114440918
-
-var OpenStreetMap = L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }
-);
-
-window.onload = function () {
-  var map = L.map("map", {
-    center: [lat, long],
-    zoom: 13,
-    maxBounds: [
-      [-120, -220],
-      [120, 220],
-    ],
-    layers: [OpenStreetMap],
-  });
-
-  var baseMaps = {
-    "OSM Mapnick": OpenStreetMap,
-  };
-
-  L.control.layers(baseMaps).addTo(map);
-
-  map.addLayer(OpenStreetMap);
-
-}
-
+  let lat = 5
+  let lon = 7
+  
   // currency exchange api - exchange
   const options2 = {
     method: "GET",
@@ -107,3 +75,68 @@ $.getJSON(URL, function (data) {
     });
   else console.log("No hits");
 });
+
+
+document.querySelector("#submit-form").addEventListener("submit", function(event){
+    event.preventDefault()
+
+    let apiKey = "b5657f205b6b0f7351867ba9e56f2a2c"
+
+    let searchInput = $("#searchCity").val()
+    
+    let countryQueryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=${apiKey}`
+
+
+    $.ajax({url : countryQueryUrl}).then(function(response){
+        lat = response[0].lat
+        lon = response[0].lon
+        
+        let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`
+
+
+        $.ajax({url : queryUrl}).then(function(weatherResponse){   
+
+            const weatherList = weatherResponse.list
+            const weatherToday = weatherList[0]
+            
+            document.querySelector("#temp").append( "Temperature: " + weatherToday.wind.speed)
+            document.querySelector("#wind").append("Wind: " + weatherToday.main.temp )
+            document.querySelector("#humidity").append("Humidity: " + weatherToday.main.humidity)
+            
+        })
+    })
+    
+})
+
+console.log(lat, lon)
+
+// Leaflet api for map
+var OpenStreetMap = L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }
+  );
+  
+  window.onload = function () {
+    var map = L.map("map", {
+      center: [lat, lon],
+      zoom: 13,
+      maxBounds: [
+        [-120, -220],
+        [120, 220],
+      ],
+      layers: [OpenStreetMap],
+    });
+  
+    var baseMaps = {
+      "OSM Mapnick": OpenStreetMap,
+    };
+  
+    L.control.layers(baseMaps).addTo(map);
+  
+    map.addLayer(OpenStreetMap);
+  
+  }
